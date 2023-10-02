1e4be7ddbba932807bb0e2f70314e3b636be6fea
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2023-10-02 17:47:03 +0300

    Added visual features as described in section 3

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        ProgressView(value: 10, total: 15)
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack{
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "backward.fill")
+                }
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
     }
 }
 
