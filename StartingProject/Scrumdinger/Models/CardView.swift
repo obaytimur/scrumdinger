0b7029756c890d6dfaf9c2da78c5397baba7d1ba
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2023-10-03 23:35:06 +0300

    tem

--- StartingProject/Scrumdinger/Models/CardView.swift
+++ StartingProject/Scrumdinger/Models/CardView.swift
@@ -13,13 +13,20 @@ struct CardView: View {
         VStack(alignment: .leading){
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(/*@START_MENU_TOKEN@*/.isHeader/*@END_MENU_TOKEN@*/)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+                    .labelStyle(.trailingIcon)
             }
+            .font(.caption)
         }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
---
+++ StartingProject/Scrumdinger/Models/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Oğulcan Baytimur on 03/10/2023.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack{
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
