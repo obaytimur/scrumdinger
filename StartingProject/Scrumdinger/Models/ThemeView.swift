943dc7c683bf8d1d012107f71c7d65c2a948afd0
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2023-10-05 17:05:39 +0300

    Finished 'passing data with binding' chapter

---
+++ StartingProject/Scrumdinger/Models/ThemeView.swift
@@ -0,0 +1,27 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Oğulcan Baytimur on 05/10/2023.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .foregroundColor(theme.accentColor)
+            .background(theme.mainColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .buttercup)
+    }
+}
--- StartingProject/Scrumdinger/ScrumdingerApp.swift
+++ StartingProject/Scrumdinger/ScrumdingerApp.swift
@@ -6,9 +6,11 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
+    @State private var scrums = DailyScrum.sampleData
+    
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: DailyScrum.sampleData)
+            ScrumsView(scrums: $scrums)
         }
     }
 }
